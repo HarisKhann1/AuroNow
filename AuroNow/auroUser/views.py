8acb9b0f34ad8903aee8c9e1fe5c33d155114ebc
Abdul Rehman <abdulrehmanafridi88@gmail.com>
2025-04-23 05:04:18 +0500

    add filter in search results

--- AuroNow/auroUser/views.py
+++ AuroNow/auroUser/views.py
@@ -80,7 +80,13 @@ def search_results(request):
                 'rating': str(round(uniform(3.5, 5.0), 1)),
             })
 
-    return render(request, 'search_results.html', {'results': results})
+    return render(request, 'search_results.html', {
+        'results': results,
+        'categories': ServiceCategory.objects.values_list('name', flat=True).distinct(),
+        'addresses': ShopOwner.objects.values_list('address', flat=True).distinct(),
+        'price_ranges': ["0-50", "51-100", "101-200", "200+"],
+    });
+    # return render(request, 'search_results.html', {'results': results})
 
 def shop_detail(request, email):
     shop = get_object_or_404(ShopOwner, email=email)
